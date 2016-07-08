9d71fb89d2597c178444d8e2d0c19b58a5b82f98
Conrad Dean <conrad.p.dean@gmail.com>
2016-07-08 15:56:40 -0400

    switch to actual impl. make test generators smaller

--- setcoverage/__init__.py
+++ setcoverage/__init__.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from itertools import combinations, chain
 
 
 def _without(objects, i):
@@ -27,3 +28,21 @@ def reduce_sets(sets):
     for n in range(len(sets)):
         o = _prune_one(o)
     return o
+
+
+def brute_force_set_coverage(sets):
+    """
+    most naiive approach to set coverage.
+
+    Try all solutions
+    Filter non-covering solutions
+    Choose smallest solution
+    """
+    o = [s for s in sets if s != set([])]
+    if len(o) == 0:
+        return o
+    goal_set = _union_all(o)
+    solution_generators = [combinations(o, i + 1) for i in range(len(o))]
+    all_possible_solutions = chain.from_iterable(solution_generators)
+    valid_solutions = [i for i in all_possible_solutions if _union_all(i) == goal_set]
+    return list(sorted((i for i in valid_solutions), key=len)[0])
--- tests/test_thing.py
+++ tests/test_thing.py
@@ -2,38 +2,48 @@ import hypothesis as h
 from hypothesis import strategies as st
 from functools import reduce
 
-from setcoverage import reduce_sets
+from setcoverage import reduce_sets, brute_force_set_coverage
 
 
 def test_example():
     s = [{1}, {1, 2}, {2}]
-    assert reduce_sets(s) == [{1, 2}]
+    assert brute_force_set_coverage(s) == [{1, 2}]
 
 
-@h.given(st.lists(st.sets(st.integers())))
+def list_of_sets():
+    return st.lists(
+        st.sets(
+            st.integers(max_value=8),
+            max_size=8
+        ),
+        max_size=16
+    )
+
+
+@h.given(list_of_sets())
 def test_that_output_has_fewer_sets_than_input(sets):
-    output = reduce_sets(sets)
+    output = brute_force_set_coverage(sets)
     assert len(output) <= len(sets)
 
 
-@h.given(st.lists(st.sets(st.integers())))
+@h.given(list_of_sets())
 def test_no_items_lost(sets):
     union = lambda a, b: a.union(b)
     all_input = reduce(union, sets, set())
-    all_output = reduce(union, reduce_sets(sets), set())
+    all_output = reduce(union, brute_force_set_coverage(sets), set())
     assert all_input == all_output
 
 
-@h.given(st.lists(st.sets(st.integers())))
+@h.given(list_of_sets())
 def test_all_sets_come_from_input(sets):
-    output = reduce_sets(sets)
+    output = brute_force_set_coverage(sets)
     forbidden_sets = [s for s in output if s not in sets]
     assert len(forbidden_sets) == 0
 
 
-@h.given(st.lists(st.sets(st.integers())))
+@h.given(list_of_sets())
 def test_no_duplicate_sets(sets):
-    output = reduce_sets(sets)
+    output = brute_force_set_coverage(sets)
     for e_ix, e in enumerate(output):
         for s_ix, s in enumerate(output):
             if e_ix == s_ix:
@@ -41,15 +51,15 @@ def test_no_duplicate_sets(sets):
             assert e != s, "duplicate in {}".format(output)
 
 
-@h.given(st.lists(st.sets(st.integers())))
+@h.given(list_of_sets())
 def test_no_empty_sets(sets):
-    output = reduce_sets(sets)
+    output = brute_force_set_coverage(sets)
     assert set([]) not in output
 
 
-@h.given(st.lists(st.sets(st.integers())))
+@h.given(list_of_sets())
 def test_no_subsets(sets):
-    output = reduce_sets(sets)
+    output = brute_force_set_coverage(sets)
     for e_ix, e in enumerate(output):
         for s_ix, s in enumerate(output):
             if e_ix == s_ix:
@@ -57,12 +67,12 @@ def test_no_subsets(sets):
             assert not e.issubset(s), "subset found in {}".format(output)
 
 
-@h.given(st.lists(st.sets(st.integers(min_value=-1, max_value=5), max_size=3), max_size=8))
-@h.settings(max_examples=2000)
+@h.given(list_of_sets())
+@h.settings(max_examples=200)
 def test_no_sibling_superset_cover_a_set_in_output(sets):
     union = lambda a, b: a.union(b)
     without = lambda o, i: o[:i] + o[i + 1:]
-    output = reduce_sets(sets)
+    output = brute_force_set_coverage(sets)
     for i, e in enumerate(output):
         siblings_superset = reduce(union, without(output, i), set())
         assert not siblings_superset.issuperset(e)
@@ -73,4 +83,4 @@ def test_hard_coverage():
         {1,   2}, {3,   4},
         {1}, {2,   3}, {4}
     ]
-    assert reduce_sets(s) == [{1, 2}, {3, 4}]
+    assert brute_force_set_coverage(s) == [{1, 2}, {3, 4}]
