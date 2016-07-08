e9034751381a40e6b173cd3c3621599f21abc0ff
Conrad Dean <conrad.p.dean@gmail.com>
2016-07-08 11:31:40 -0400

    implement element coverage property

--- tests/test_thing.py
+++ tests/test_thing.py
@@ -1,5 +1,6 @@
 import hypothesis as h
 from hypothesis import strategies as st
+from functools import reduce
 
 from setcoverage import reduce_sets
 
@@ -13,3 +14,11 @@ def test_example():
 def test_that_output_has_fewer_sets_than_input(sets):
     output = reduce_sets(sets)
     assert len(output) <= len(sets)
+
+
+@h.given(st.lists(st.sets(st.integers())))
+def test_no_items_lost(sets):
+    union = lambda a, b: a.union(b)
+    all_input = reduce(union, sets, set())
+    all_output = reduce(union, reduce_sets(sets), set())
+    assert all_input == all_output
