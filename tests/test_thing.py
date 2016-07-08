f8a1082a5625505d30a40f4f2c08088164df4e65
Conrad Dean <conrad.p.dean@gmail.com>
2016-07-08 10:26:27 -0400

    first property of set coverage

--- tests/test_thing.py
+++ tests/test_thing.py
@@ -1,6 +1,15 @@
+import hypothesis as h
+from hypothesis import strategies as st
+
 import setcoverage
 
 
 def test_example():
     s = [{1}, {1, 2}, {2}]
     assert setcoverage.reduce_sets(s) == [{1, 2}]
+
+
+@h.given(st.lists(st.sets(st.integers())))
+def test_that_output_has_fewer_sets_than_input(sets):
+    output = setcoverage.reduce_sets(sets)
+    assert len(output) <= len(sets)
