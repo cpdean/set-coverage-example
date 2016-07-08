41045b5aba2ae7d9e7f808759a27d8f50d49d38a
Conrad Dean <conrad.p.dean@gmail.com>
2016-07-08 10:23:42 -0400

    minimum viable product

---
+++ setcoverage/__init__.py
@@ -0,0 +1,3 @@
+
+def reduce_sets(sets):
+    return [{1, 2}]
--- tests/test_thing.py
+++ tests/test_thing.py
@@ -1,2 +1,6 @@
-def test_thing():
-    assert 1 == 2
+import setcoverage
+
+
+def test_example():
+    s = [{1}, {1, 2}, {2}]
+    assert setcoverage.reduce_sets(s) == [{1, 2}]
